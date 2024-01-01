5e65d843ce3c7bc14db7dfc24520fdae3875f511
Kifah Hassan <26527136+kifahhassan@users.noreply.github.com>
2023-12-31 19:09:20 -0600

    added function that returns rock, paper, or scissors, based on a random number generator from 1 to 3

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello, this is a test for JavaScript");
+
+function getComputerChoice(){
+    var number = Math.floor(Math.random() * 3) + 1;
+
+switch (number) {
+    case 1:
+        return "Rock";
+    case 2:
+        return "Paper";
+    case 3:
+        return "Scissors";
+    default:
+        return "The system produced an error. Please try again"; //This handles unexpected values
+}
+
+}
