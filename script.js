a6915ce576873ca6f5f6fa704a457d1116594256
Kifah Hassan <26527136+kifahhassan@users.noreply.github.com>
2024-01-02 19:15:54 -0600

    modified playround function in order to track score, and started game function

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 console.log("Hello, this is a test for JavaScript");
 
 function getComputerChoice(){
-    var number = Math.floor(Math.random() * 3) + 1;
+    let number = Math.floor(Math.random() * 3) + 1;
 
 switch (number) {
     case 1:
-        return "Rock";
+        return "ROCK";
     case 2:
-        return "Paper";
+        return "PAPER";
     case 3:
-        return "Scissors";
+        return "SCISSORS";
     default:
         return "The system produced an error. Please try again"; //This handles unexpected values
 }
-
 }
+playerScore = 0;
+computerScore = 0;
+roundWinner = "";
 
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection === computerSelection) {
-        return "it's a tie!";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
+    if (playerSelection === "rock" && computerSelection === "paper") {
         return "You win! Rock beats paper";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You lose! Rock beats paper!";
@@ -32,8 +32,35 @@ function playRound(playerSelection, computerSelection) {
         return "You win! Scissors beats rock!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose! Scissors beats rock!";
+    } else if (playerSelection === computerSelection) {
+        return "it's a tie!";
+}
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (
+        playerSelection === "PAPER" && computerSelection === "ROCK" ||
+        playerSelection === "ROCK" && computerSelection === "SCISSORS" ||
+        playerSelection === "SCISSORS" && computerSelection === "PAPER"
+        ) {
+            playerScore++;
+            roundWinner = "player";
+        }
+    if (
+        computerSelection === "PAPER" && playerSelection === "ROCK" ||
+        computerSelection === "ROCK" && playerSelection === "ROCK" ||
+        computerSelection === "SCISSORS" && playerSelection === "PAPER"
+    ) {
+        computerScore++;
+        roundWinner = "computer";
     }
 }
 
-var playerSelection = prompt("Let's play rock, paper, scissors.").toLowerCase();
 const computerSelection = getComputerChoice().toLowerCase();
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Let's play rock, paper, scissors.").toLowerCase();
+        playRound(playerSelection, computerSelection);
+    }
+}
